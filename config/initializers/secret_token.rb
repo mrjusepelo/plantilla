12618e638c6900e605779a7fd4c6648e05d9f675
mrjusepelo <jperezlondono@gmail.com>
2013-09-02 22:53:12 -0500

    plantilla commit 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plantilla::Application.config.secret_key_base = 'd8f10c586e4679bc5cddfafa6fc0a252bfff15844234266329de0485e83babc84d1d61f46804da2c1ff3f317ae3d5da0b488606ee52385b2f088c28eef299f61'
